1762373e3beb7dc2414d2a5397bea417e699734f
Halvor Ødegård Teigen <halvor1996@hotmail.com>
2021-01-13 16:13:12 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 
 from setuptools import setup
 
-setup(name='gym-turbine',
-      version='0.0.1',
-      python_requires='>=3.7',
-      install_requires=[
-          'ray[rllib]',
-          'gym'
-          ]  # And any other dependencies it needs
-      )
+setup(
+    name='gym-turbine',
+    version='0.0.1',
+    python_requires='>=3.7, <=3.8',
+    url='https://github.com/halvorot/gym-turbine',
+    install_requires=[
+        'ray[rllib]',
+        'gym'
+    ]  # And any other dependencies it needs
+)

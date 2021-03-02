38ea918bac920ffb60b49cbbea0e8e529bbc3b2c
halvorot <halvor1996@hotmail.com>
2021-03-02 16:15:35 +0100

    reduced max input

--- gym_rl_mpc/utils/model_params.py
+++ gym_rl_mpc/utils/model_params.py
@@ -11,4 +11,4 @@ k = 1e6
 zeta = 1
 c = 2*zeta*np.sqrt(m*k)
 J = (1/12)*m*(L**2 + 2*L*L_P + 6*L + L_P**2 - 6*L_P)
-max_input = 1e7                     # maximum force input [N]
+max_input = 5e6                     # maximum force input [N]

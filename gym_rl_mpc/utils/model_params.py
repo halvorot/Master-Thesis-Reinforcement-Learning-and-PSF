aa7a56bc97fcce8625a7e999cc8980880dc74ef3
halvorot <halvor1996@hotmail.com>
2021-03-03 17:23:09 +0100

    tuned model params to match FAST response

--- gym_rl_mpc/utils/model_params.py
+++ gym_rl_mpc/utils/model_params.py
@@ -6,11 +6,12 @@ DoFs = 1
 # Turbine parameters
 L = 144.495
 L_P = 50
-L_COM = 10                          # Meters below rotation point
+L_COM = 6                          # Meters below rotation point
 m = 1.7838E+07 + 190000 + 507275    # Platform mass + Hub mass + Nacelle mass 
-k = 8e5
-c = 632455.53
-J = 1.2507E+10 + m*(L_COM)**2       # J_COM + m*d^2
+k = 4.5e4
+zeta = 1
+c = 2*zeta*np.sqrt(m*k)
+J = 2*1.2507E+10 + m*(L_COM)**2       # J_COM + m*d^2
 max_input = 1e6                     # maximum force input [N]
 omega_disturbance = 1
 max_disturbance = 1e7

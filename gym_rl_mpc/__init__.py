e01222885f4119a2677792a42749f826847be101
halvorot <halvor1996@hotmail.com>
2021-03-03 17:36:54 +0100

    changed timestep to 1

--- gym_rl_mpc/__init__.py
+++ gym_rl_mpc/__init__.py
@@ -2,9 +2,9 @@ from gym.envs.registration import register
 import numpy as np
 
 DEFAULT_CONFIG = {
-    "step_size": 0.01,
+    "step_size": 1,
     "min_reward": -1500,
-    "max_t_steps": 30000,
+    "max_t_steps": 3000,
     "crash_angle_condition": 7*(np.pi/180),
     "max_init_angle": 3*(np.pi/180),
     "reward_crash": -2000,

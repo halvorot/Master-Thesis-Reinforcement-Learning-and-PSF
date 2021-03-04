8df51d4bc183eea7b9ba01cd4711ce1b310eda07
halvorot <halvor1996@hotmail.com>
2021-03-04 09:26:49 +0100

    add wind_speed var, no actual chng in running code

--- gym_rl_mpc/objects/pendulum.py
+++ gym_rl_mpc/objects/pendulum.py
@@ -2,7 +2,7 @@ import numpy as np
 import gym_rl_mpc.utils.model_params as params
 import gym_rl_mpc.utils.geomutils as geom
 
-def odesolver45(f, y, h):
+def odesolver45(f, y, h, wind_speed):
     """Calculate the next step of an IVP of a time-invariant ODE with a RHS
     described by f, with an order 4 approx. and an order 5 approx.
     Parameters:
@@ -13,12 +13,12 @@ def odesolver45(f, y, h):
         q: float. Order 4 approx.
         w: float. Order 5 approx.
     """
-    s1 = f(y)
-    s2 = f(y + h*s1/4.0)
-    s3 = f(y + 3.0*h*s1/32.0 + 9.0*h*s2/32.0)
-    s4 = f(y + 1932.0*h*s1/2197.0 - 7200.0*h*s2/2197.0 + 7296.0*h*s3/2197.0)
-    s5 = f(y + 439.0*h*s1/216.0 - 8.0*h*s2 + 3680.0*h*s3/513.0 - 845.0*h*s4/4104.0)
-    s6 = f(y - 8.0*h*s1/27.0 + 2*h*s2 - 3544.0*h*s3/2565 + 1859.0*h*s4/4104.0 - 11.0*h*s5/40.0)
+    s1 = f(y, wind_speed)
+    s2 = f(y + h*s1/4.0, wind_speed)
+    s3 = f(y + 3.0*h*s1/32.0 + 9.0*h*s2/32.0, wind_speed)
+    s4 = f(y + 1932.0*h*s1/2197.0 - 7200.0*h*s2/2197.0 + 7296.0*h*s3/2197.0, wind_speed)
+    s5 = f(y + 439.0*h*s1/216.0 - 8.0*h*s2 + 3680.0*h*s3/513.0 - 845.0*h*s4/4104.0, wind_speed)
+    s6 = f(y - 8.0*h*s1/27.0 + 2*h*s2 - 3544.0*h*s3/2565 + 1859.0*h*s4/4104.0 - 11.0*h*s5/40.0, wind_speed)
     w = y + h*(25.0*s1/216.0 + 1408.0*s3/2565.0 + 2197.0*s4/4104.0 - s5/5.0)
     q = y + h*(16.0*s1/135.0 + 6656.0*s3/12825.0 + 28561.0*s4/56430.0 - 9.0*s5/50.0 + 2.0*s6/55.0)
     return w, q
@@ -34,21 +34,21 @@ class Pendulum():
         self.F_d = 0
         self.disturbance_phase_offset = np.random.normal(0,0.5*np.pi)
 
-    def step(self, action):
+    def step(self, action, wind_speed):
         F = _un_normalize_input(action)
         self.input = float(F)
 
-        self._sim()
+        self._sim(wind_speed)
         self.t_step += 1
 
-    def _sim(self):
+    def _sim(self, wind_speed):
 
-        state_o5, state_o4 = odesolver45(self.state_dot, self.state, self.step_size)
+        state_o5, state_o4 = odesolver45(self.state_dot, self.state, self.step_size, wind_speed)
 
         self.state = state_o5
         self.state[0] = geom.ssa(self.state[0])
 
-    def state_dot(self, state):
+    def state_dot(self, state, wind_speed):
         """
         state = [theta, theta_dot, omega]^T
         """

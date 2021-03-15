f6aa28584c0837f786d0b5d36f811706892bb4f3
halvorot <halvor1996@hotmail.com>
2021-03-15 13:43:40 +0100

    changed print

--- gym_rl_mpc/objects/pendulum.py
+++ gym_rl_mpc/objects/pendulum.py
@@ -41,8 +41,8 @@ class Pendulum:
                                                        power=init_power,
                                                        # needs a param
                                                        thruster_force=0)
-        print(float(blade_pitch))
-        print(opt_state)
+        print(f"init blade pitch: {float(blade_pitch)}")
+        print(f"opt_state: {opt_state}")
         self.state = opt_state
         self.input = np.array([0, float(blade_pitch), init_power])  # Initialize control input
         self.step_size = step_size

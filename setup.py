923aec03cd88fd98dd4a6e1b72d0ee04e4045ded
halvorot <halvor1996@hotmail.com>
2021-03-16 11:17:13 +0100

    changed version requirements for packages

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from setuptools import setup
 setup(
     name='gym-rl-mpc',
     version='0.0.1',
-    python_requires='>=3.7, <3.9',
+    python_requires='>=3.7, <3.8',
     url='https://github.com/halvorot/gym-rl-mpc',
     install_requires=[
+        'numpy>=1.19.2,<1.20',
         'gym',
         'stable-baselines3[extra]',
         'matplotlib',

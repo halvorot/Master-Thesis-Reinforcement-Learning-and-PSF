bdc9aba3059afa23998bece3f42faac66d24574d
halvorot <halvor1996@hotmail.com>
2021-04-08 20:38:38 +0200

    changed to 15M in train script

--- script_train.bat
+++ script_train.bat
@@ -1,5 +1,5 @@
 @echo off
-python train.py --timesteps 20000000 --env VariableWindLevel0-v0
-python train.py --timesteps 20000000 --env VariableWindLevel1-v0
-python train.py --timesteps 20000000 --env VariableWindLevel2-v0
-python train.py --timesteps 20000000 --env VariableWindLevel3-v0
+python train.py --timesteps 15000000 --env VariableWindLevel0-v0
+python train.py --timesteps 15000000 --env VariableWindLevel1-v0
+python train.py --timesteps 15000000 --env VariableWindLevel2-v0
+python train.py --timesteps 15000000 --env VariableWindLevel3-v0

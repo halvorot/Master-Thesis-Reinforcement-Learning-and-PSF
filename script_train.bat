615e7f7346e7a91e7b92961ae66b4e0144a0be50
halvorot <halvor1996@hotmail.com>
2021-04-09 13:30:54 +0200

    lowered num timesteps in trian scripts

--- script_train.bat
+++ script_train.bat
@@ -1,5 +1,5 @@
 @echo off
-python train.py --timesteps 10000000 --env VariableWindLevel0-v0 --note "learning rate linear schedule, init_value=1e-4"
-python train.py --timesteps 10000000 --env VariableWindLevel1-v0 --note "learning rate linear schedule, init_value=1e-4"
-python train.py --timesteps 10000000 --env VariableWindLevel2-v0 --note "learning rate linear schedule, init_value=1e-4"
-python train.py --timesteps 10000000 --env VariableWindLevel3-v0 --note "learning rate linear schedule, init_value=1e-4"
+python train.py --timesteps 5000000 --env VariableWindLevel0-v0 --note "learning rate linear schedule, init_value=1e-4"
+python train.py --timesteps 5000000 --env VariableWindLevel1-v0 --note "learning rate linear schedule, init_value=1e-4"
+python train.py --timesteps 5000000 --env VariableWindLevel2-v0 --note "learning rate linear schedule, init_value=1e-4"
+python train.py --timesteps 5000000 --env VariableWindLevel3-v0 --note "learning rate linear schedule, init_value=1e-4"

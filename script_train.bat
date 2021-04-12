8543dd3526eece3957c5576f6b39f1b85b306843
halvorot <halvor1996@hotmail.com>
2021-04-12 21:18:22 +0200

    changed script files

--- script_train.bat
+++ script_train.bat
@@ -2,7 +2,4 @@
 python train.py --timesteps 5000000 --env VariableWindLevel0-v16 --note "learning rate linear schedule, init_value=1e-4"
 python train.py --timesteps 5000000 --env VariableWindLevel1-v16 --note "learning rate linear schedule, init_value=1e-4"
 python train.py --timesteps 5000000 --env VariableWindLevel2-v16 --note "learning rate linear schedule, init_value=1e-4"
-python train.py --timesteps 3000000 --env VariableWindLevel0-v16 --psf --note "learning rate linear schedule, init_value=1e-4"
-python train.py --timesteps 3000000 --env VariableWindLevel1-v16 --psf --note "learning rate linear schedule, init_value=1e-4"
-python train.py --timesteps 3000000 --env VariableWindLevel2-v16 --psf --note "learning rate linear schedule, init_value=1e-4"
-python train.py --timesteps 3000000 --env VariableWindLevel3-v16 --psf --note "learning rate linear schedule, init_value=1e-4"
+python train.py --timesteps 5000000 --env VariableWindLevel8-v16 --note "learning rate linear schedule, init_value=1e-4"
---
+++ script_train_psf.bat
@@ -0,0 +1,5 @@
+@echo off
+python train.py --timesteps 3000000 --env VariableWindLevel0-v16 --psf --note "learning rate linear schedule, init_value=1e-4"
+python train.py --timesteps 3000000 --env VariableWindLevel1-v16 --psf --note "learning rate linear schedule, init_value=1e-4"
+python train.py --timesteps 3000000 --env VariableWindLevel2-v16 --psf --note "learning rate linear schedule, init_value=1e-4"
+python train.py --timesteps 3000000 --env VariableWindLevel3-v16 --psf --note "learning rate linear schedule, init_value=1e-4"

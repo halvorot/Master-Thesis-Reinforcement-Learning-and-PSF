4bc5e4aaebb123a7028fb3179ea1f0d9e3cb6d91
halvorot <halvor1996@hotmail.com>
2021-04-22 09:16:56 +0200

    new generalization plots

--- plot_scripts/plot_generalization_results.py
+++ plot_scripts/plot_generalization_results.py
@@ -2,46 +2,56 @@ import argparse
 
 import plotly.io as pio
 import plotly.graph_objects as go
+import numpy as np
 
 font_dict=dict(family='Arial',
             size=18,
             color='black'
             )
 
-def plot_gen_performance(save=False):
+def plot_gen_performance(save=False, group_by_test_level=False):
 
     xlabels = ['Level 0', 'Level 1', 'Level 2', 'Level 3', 'Level 4', 'Level 5']
-
+    data = np.array([[6193.86, 6073.30, 5867.92, 6108.16, 5855.94, 5815.67], 
+                     [6184.24, 6043.18, 5736.11, 6060.25, 5791.88, 5681.88],
+                     [6041.56,5952.68,5163.92,5953.56,5783.19,5598.68],
+                     [6156.46,6098.61,5772.30,6100.28,5850.98,5837.25],
+                     [6163.32,6055.70,5835.18,6089.63,5883.88,5760.50],
+                     [0,0,0,0,0,0]
+                    ])
+    if group_by_test_level:
+        data = data.transpose()
+    
     fig = go.Figure()
     fig.add_trace(go.Bar(
         x=xlabels,
-        y=[6193.86,6184.24,6041.56,6156.46,1,1],
-        name='Level 0',
+        y=data[:,0],
+        name='Agent Level 0' if group_by_test_level else 'Test Level 0',
     ))
     fig.add_trace(go.Bar(
         x=xlabels,
-        y=[6073.30,6043.18,5952.68,6098.61,1,1],
-        name='Level 1',
+        y=data[:,1],
+        name='Agent Level 1' if group_by_test_level else 'Test Level 1',
     ))
     fig.add_trace(go.Bar(
         x=xlabels,
-        y=[5867.92,5736.11,5163.92,5772.30,1,1],
-        name='Level 2',
+        y=data[:,2],
+        name='Agent Level 2' if group_by_test_level else 'Test Level 2',
     ))
     fig.add_trace(go.Bar(
         x=xlabels,
-        y=[6108.16,6060.25,5953.56,6100.28,1,1],
-        name='Level 3',
+        y=data[:,3],
+        name='Agent Level 3' if group_by_test_level else 'Test Level 3',
     ))
     fig.add_trace(go.Bar(
         x=xlabels,
-        y=[5855.94,5791.88,5783.19,5850.98,0,1],
-        name='Level 4',
+        y=data[:,4],
+        name='Agent Level 4' if group_by_test_level else 'Test Level 4',
     ))
     fig.add_trace(go.Bar(
         x=xlabels,
-        y=[5815.67,5681.88,5598.68,5837.25,1,0],
-        name='Level 5',
+        y=data[:,5],
+        name='Agent Level 5' if group_by_test_level else 'Test Level 5',
     ))
 
     fig.update_layout(  barmode='group', 
@@ -60,7 +70,7 @@ def plot_gen_performance(save=False):
                     tickwidth=2.4,  # tick width
                     tickcolor='black',  # tick color
                     )
-    fig.update_xaxes(title_text='Agent Train Level',
+    fig.update_xaxes(title_text='Test Level' if group_by_test_level else 'Agent Train Level',
                     showline=True,
                     showticklabels=True,
                     linecolor='black',
@@ -75,42 +85,55 @@ def plot_gen_performance(save=False):
     fig.show()
 
     if save:
-        fig.write_image("plots/generalization_performance_bar.pdf")
+        if group_by_test_level:
+            fig.write_image("plots/generalization_performance_bar_group_by_test_level.pdf")
+        else:
+            fig.write_image("plots/generalization_performance_bar_group_by_agent_level.pdf")
 
-def plot_gen_crash(save=False):
+def plot_gen_crash(save=False, group_by_test_level=False):
 
     xlabels = ['Level 0', 'Level 1', 'Level 2', 'Level 3', 'Level 4', 'Level 5']
 
+    data = np.array([   [0,0,22,0,0,23], 
+                        [0,0,25,0,0,19],
+                        [0,6,14,1,0,17],
+                        [0,0,22,0,0,31],
+                        [0,0,23,0,0,26],
+                        [0,0,0,0,0,0]
+                ])
+    if group_by_test_level:
+        data = data.transpose()
+    
     fig = go.Figure()
     fig.add_trace(go.Bar(
         x=xlabels,
-        y=[0,0,0,0,0,0],
-        name='Level 0',
+        y=data[:,0],
+        name='Agent Level 0' if group_by_test_level else 'Test Level 0',
     ))
     fig.add_trace(go.Bar(
         x=xlabels,
-        y=[0,0,6,0,0,0],
-        name='Level 1',
+        y=data[:,1],
+        name='Agent Level 1' if group_by_test_level else 'Test Level 1',
     ))
     fig.add_trace(go.Bar(
         x=xlabels,
-        y=[22,25,14,22,0,0],
-        name='Level 2',
+        y=data[:,2],
+        name='Agent Level 2' if group_by_test_level else 'Test Level 2',
     ))
     fig.add_trace(go.Bar(
         x=xlabels,
-        y=[0,0,1,0,0,0],
-        name='Level 3',
+        y=data[:,3],
+        name='Agent Level 3' if group_by_test_level else 'Test Level 3',
     ))
     fig.add_trace(go.Bar(
         x=xlabels,
-        y=[0,0,0,0,0,0],
-        name='Level 4',
+        y=data[:,4],
+        name='Agent Level 4' if group_by_test_level else 'Test Level 4',
     ))
     fig.add_trace(go.Bar(
         x=xlabels,
-        y=[23,19,17,31,0,0],
-        name='Level 5',
+        y=data[:,5],
+        name='Agent Level 5' if group_by_test_level else 'Test Level 5',
     ))
 
     fig.update_layout(  barmode='group', 
@@ -130,7 +153,7 @@ def plot_gen_crash(save=False):
                     tickcolor='black',  # tick color
                     range=[0, 100],
                     )
-    fig.update_xaxes(title_text='Agent Train Level',
+    fig.update_xaxes(title_text='Test Level' if group_by_test_level else 'Agent Train Level',
                     showline=True,
                     showticklabels=True,
                     linecolor='black',
@@ -145,7 +168,10 @@ def plot_gen_crash(save=False):
     fig.show()
 
     if save:
-        fig.write_image("plots/generalization_crash_bar.pdf")
+        if group_by_test_level:
+            fig.write_image("plots/generalization_crash_bar_group_by_test_level.pdf")
+        else:
+            fig.write_image("plots/generalization_crash_bar_group_by_agent_level.pdf")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -154,7 +180,12 @@ if __name__ == '__main__':
         help='Save plots',
         action='store_true'
     )
+    parser.add_argument(
+        '--group_by_test_level',
+        help='Group bars by test level instead of agent level',
+        action='store_true'
+    )
     args = parser.parse_args()
 
-    plot_gen_performance(save=args.save)
-    plot_gen_crash(save=args.save)
+    plot_gen_performance(save=args.save,group_by_test_level=args.group_by_test_level)
+    plot_gen_crash(save=args.save,group_by_test_level=args.group_by_test_level)

27ef995e92cd7b5be00cd68f5e3aa75da1a3dae7
Vebjørn Malmin <vebjorma@stud.ntnu.no>
2021-04-26 13:54:56 +0200

    Resolved error in PSF, and changed the PSF bat script. Running now

--- script_train_psf.bat
+++ script_train_psf.bat
@@ -1,5 +1,5 @@
 @echo off
-python train.py --timesteps 2000000 --env VariableWindLevel1-v17 --psf --psf_T 20 --psf_lb_omega 0.41887902048 --psf_ub_omega 0.94247779608 --note "learning rate linear schedule, init_value=1e-4,--T 20 --psf-bounds 4-9 --note"
-python train.py --timesteps 2000000 --env VariableWindLevel1-v17 --psf --psf_T 40 --note "learning rate linear schedule, init_value=1e-4, --T 40 --psf-bounds 5-7.6"
-python train.py --timesteps 2000000 --env VariableWindLevel1-v17 --psf --psf_T 5 --note "learning rate linear schedule, init_value=1e-4, --T 5 --psf-bounds 5-7.6"
-python train.py --timesteps 2000000 --env VariableWindLevel1-v17 --pgit sf - --note "learning rate linear schedule, init_value=1e-4, --T 20 --psf-bounds 5-7.6"
+python train.py --timesteps 2000000 --env VariableWindLevel1-v17 --psf --psf_T 5 --note "Horizon results . psf_T 5"
+python train.py --timesteps 2000000 --env VariableWindLevel1-v17 --psf --psf_T 10 --note "Horizon results . psf_T 10"
+python train.py --timesteps 2000000 --env VariableWindLevel1-v17 --psf --psf_T 20 --note "Horizon results . psf_T 20"
+python train.py --timesteps 2000000 --env VariableWindLevel1-v17 --psf --psf_T 40 --note "Horizon results . psf_T 40"

9d037073486ebf61602264d591131b00afbe70f8
Vebjørn Malmin <vebjorma@stud.ntnu.no>
2021-04-28 17:13:03 +0200

    updated bat files

--- script_train.bat
+++ script_train.bat
@@ -1,8 +1,8 @@
 @echo off
-python train.py --timesteps 10000000 --env VariableWindLevel0-v17 --note "learning rate linear schedule, init_value=1e-4"
-python train.py --timesteps 10000000 --env VariableWindLevel0-v17 --note "learning rate linear schedule, init_value=1e-4"
-python train.py --timesteps 10000000 --env VariableWindLevel0-v17 --note "learning rate linear schedule, init_value=1e-4"
-python train.py --timesteps 10000000 --env VariableWindLevel0-v13 --note "learning rate linear schedule, init_value=1e-4"
-python train.py --timesteps 10000000 --env VariableWindLevel0-v14 --note "learning rate linear schedule, init_value=1e-4"
-python train.py --timesteps 10000000 --env VariableWindLevel0-v15 --note "learning rate linear schedule, init_value=1e-4"
-python train.py --timesteps 10000000 --env VariableWindLevel0-v16 --note "learning rate linear schedule, init_value=1e-4"
+rem python train.py --timesteps 4000000 --env VariableWindLevel10-v17 --note "error zone"
+rem python train.py --timesteps 4000000 --env VariableWindLevel11-v17 --note "error zone"
+rem python train.py --timesteps 4000000 --env VariableWindLevel12-v17 --note "error zone"
+
+ python train.py --timesteps 4000000 --env VariableWindLevel13-v17 --note "error zone"
+ python train.py --timesteps 4000000 --env VariableWindLevel14-v17 --note "error zone"
+
--- script_train_psf.bat
+++ script_train_psf.bat
@@ -1,13 +1,18 @@
 @echo off
 
 rem running on NTNU12382 26.04.2021
-python train.py --timesteps 2000000 --env VariableWindLevel1-v17 --psf --psf_T 5 --note "Horizon results . psf_T 5"
-python train.py --timesteps 2000000 --env VariableWindLevel1-v17 --psf --psf_T 10 --note "Horizon results . psf_T 10"
-python train.py --timesteps 2000000 --env VariableWindLevel1-v17 --psf --psf_T 20 --note "Horizon results . psf_T 20"
-python train.py --timesteps 2000000 --env VariableWindLevel1-v17 --psf --psf_T 40 --note "Horizon results . psf_T 40"
+rem python train.py --timesteps 2000000 --env VariableWindLevel1-v17 --psf --psf_T 5 --note "Horizon results . psf_T 5"
+rem python train.py --timesteps 2000000 --env VariableWindLevel1-v17 --psf --psf_T 10 --note "Horizon results . psf_T 10"
+rem python train.py --timesteps 2000000 --env VariableWindLevel1-v17 --psf --psf_T 20 --note "Horizon results . psf_T 20"
+rem python train.py --timesteps 2000000 --env VariableWindLevel1-v17 --psf --psf_T 40 --note "Horizon results . psf_T 40"
 
 rem running on NTNU16535 26.04.2021
 rem python train.py --timesteps 2000000 --env VariableWindLevel10-v17 --psf --psf_T 10 --note "Horizon results . psf_T 10"
 rem python train.py --timesteps 2000000 --env VariableWindLevel10-v17 --psf --psf_T 20 --note "Horizon results . psf_T 20"
 
+rem running somewhere
+rem python train.py --timesteps 2000000 --env VariableWindLevel11-v17 --psf --note "error zone"
+rem python train.py --timesteps 2000000 --env VariableWindLevel12-v17 --psf --note "error zone"
 
+python train.py --timesteps 2000000 --env VariableWindLevel13-v17 --note --psf "error zone"
+python train.py --timesteps 2000000 --env VariableWindLevel14-v17 --note --psf "error zone"

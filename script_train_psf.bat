99dd7921274246475402c23a485b84719f11833c
Vebjørn Malmin <vebjorma@stud.ntnu.no>
2021-04-28 17:25:16 +0200

    yes

--- script_train_psf.bat
+++ script_train_psf.bat
@@ -14,5 +14,5 @@ rem running somewhere
 rem python train.py --timesteps 2000000 --env VariableWindLevel11-v17 --psf --note "error zone"
 rem python train.py --timesteps 2000000 --env VariableWindLevel12-v17 --psf --note "error zone"
 
-python train.py --timesteps 2000000 --env VariableWindLevel13-v17 --note --psf "error zone"
-python train.py --timesteps 2000000 --env VariableWindLevel14-v17 --note --psf "error zone"
+python train.py --timesteps 2000000 --env VariableWindLevel13-v17 --psf --note  "error zone"
+python train.py --timesteps 2000000 --env VariableWindLevel14-v17 --psf --note  "error zone"

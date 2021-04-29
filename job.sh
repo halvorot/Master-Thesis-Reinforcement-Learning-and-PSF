12cf2b2083ddaf76d6ea1f9c23c5953b02cfe26a
halvorot <halvor1996@hotmail.com>
2021-04-29 15:29:07 +0200

    changed job to interactive

--- job.sh
+++ job.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-#SBATCH -J RL_w_PSF                   # Sensible name for the job
-#SBATCH -N 1                     # Allocate 2 nodes for the job
-#SBATCH --ntasks-per-node=1     # 1 task per node
-#SBATCH --gres=gpu:1
-#SBATCH -c 32
-#SBATCH -t 12:00:00          # Upper time limit for the job
-#SBATCH -p GPUQ
+srun --nodes=1 -c 32 --partition=GPUQ --time=24:00:00 --pty bash
 module load fosscuda/2020b
-module load Anaconda3/2020.07
-conda init
-conda env create -f environment.yml
 conda activate gym-rl-mpc
-python train.py --timesteps 3000000 --env VariableWindLevel0-v17 --psf --note "learning rate linear schedule, init_value=1e-4"
+python train.py --timesteps 10000000 --env VariableWindPSFtest-v17 --psf --note "learning rate linear schedule, init_value=1e-4" --num_cpus 32

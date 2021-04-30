fe5a43ba9d82463b26ff76743018ba30cc06863b
halvorot <halvor1996@hotmail.com>
2021-04-30 11:46:57 +0200

    new plot stuff. improved time arg in test_agent

--- plot_scripts/plot_generalization_results.py
+++ plot_scripts/plot_generalization_results.py
@@ -11,21 +11,40 @@ font_dict=dict(family='Arial',
             color='black'
             )
 
-performance_data = np.array([[6193.86, 6073.30, 5867.92, 6108.16, 5855.94, 5815.67], 
-                     [6184.24, 6043.18, 5736.11, 6060.25, 5791.88, 5681.88],
-                     [6041.56,5952.68,5163.92,5953.56,5783.19,5598.68],
-                     [6156.46,6098.61,5772.30,6100.28,5850.98,5837.25],
-                     [6163.32,6055.70,5835.18,6089.63,5883.88,5760.50],
-                     [5995.40,5738.16,5138.83,5651.29,5126.57,4954.20]
-                    ])
+performance_data_3000 = np.array([  [6193.86, 6073.30, 5867.92, 6108.16, 5855.94, 5815.67], 
+                                    [6184.24, 6043.18, 5736.11, 6060.25, 5791.88, 5681.88],
+                                    [6041.56,5952.68,5163.92,5953.56,5783.19,5598.68],
+                                    [6156.46,6098.61,5772.30,6100.28,5850.98,5837.25],
+                                    [6163.32,6055.70,5835.18,6089.63,5883.88,5760.50],
+                                    [5995.40,5738.16,5138.83,5651.29,5126.57,4954.20]
+                                    ])
 
-crash_data = np.array([   [0,0,22,0,0,23], 
-                    [0,0,25,0,0,19],
-                    [0,6,14,1,0,17],
-                    [0,0,22,0,0,31],
-                    [0,0,23,0,0,26],
-                    [0,1,25,1,1,30]
-            ])
+crash_data_3000 = np.array([[0,0,22,0,0,23], 
+                            [0,0,25,0,0,19],
+                            [0,6,14,1,0,17],
+                            [0,0,22,0,0,31],
+                            [0,0,23,0,0,26],
+                            [0,1,25,1,1,30]
+                            ])
+
+# 6000 timesteps
+performance_data = np.array([   [], 
+                                [],
+                                [],
+                                [],
+                                [],
+                                []
+                                ])
+performance_data = performance_data/6000
+
+crash_data = np.array([ [], 
+                        [],
+                        [],
+                        [],
+                        [],
+                        []
+                        ])
+
 
 def plot_gen_performance(save=False, group_by_test_level=False):
     global performance_data
@@ -186,16 +205,16 @@ def plot_gen_heatmap(save=False):
 
     data = performance_data
     ylabel = "Performance"
-    filename = "plots/generalization_performance_heatmap.pdf"
-    format = "{:.0f}".format
+    filename = "plots/generalization_performance_heatmap_6000.pdf"
+    format = "{:.3f}".format
     cmap = 'autumn'
     
-    data = crash_data
-    ylabel = "Crash rate"
-    filename = "plots/generalization_crash_heatmap.pdf"
-    format = mtick.PercentFormatter(decimals=0)
-    cmap = 'autumn_r'
-    textcolors.reverse()
+    # data = crash_data
+    # ylabel = "Crash rate"
+    # filename = "plots/generalization_crash_heatmap_6000.pdf"
+    # format = mtick.PercentFormatter(decimals=0)
+    # cmap = 'autumn_r'
+    # textcolors.reverse()
     
     labels = ["Level 0", "Level 1", "Level 2", "Level 3", "Level 4", "Level 5"]
     fig, ax = plt.subplots()

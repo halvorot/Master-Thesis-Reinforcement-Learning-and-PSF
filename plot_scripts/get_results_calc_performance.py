31badabd176a24536aedd25786d8c5f05c200eb7
halvorot <halvor1996@hotmail.com>
2021-05-03 12:38:33 +0200

    changed psf agent paths in calc perf

--- plot_scripts/get_results_calc_performance.py
+++ plot_scripts/get_results_calc_performance.py
@@ -24,12 +24,12 @@ if __name__ == '__main__':
         'level5_agent': r"..\logs\VariableWindLevel5-v17\1618946704ppo",
     }
     agent_paths_psf = {
-        'level0_agent_psf': r"..\logs\VariableWindLevel0-v17\1618915245ppo",
-        'level1_agent_psf': r"..\logs\VariableWindLevel1-v17\1618921752ppo",
-        'level2_agent_psf': r"..\logs\VariableWindLevel2-v17\1618928488ppo",
-        'level3_agent_psf': r"..\logs\VariableWindLevel3-v17\1618934307ppo",
-        'level4_agent_psf': r"..\logs\VariableWindLevel4-v17\1618940658ppo",
-        'level5_agent_psf': r"..\logs\VariableWindLevel5-v17\1618946704ppo",
+        'level0_agent_psf': r"..\logs\VariableWindLevel0-v17\1619804074ppo",
+        'level1_agent_psf': r"..\logs\VariableWindLevel1-v17\1619817419ppo",
+        'level2_agent_psf': r"..\logs\VariableWindLevel2-v17\1619805498ppo",
+        'level3_agent_psf': r"..\logs\VariableWindLevel3-v17\1619826109ppo",
+        'level4_agent_psf': r"..\logs\VariableWindLevel4-v17\1619817419ppo",
+        'level5_agent_psf': r"..\logs\VariableWindLevel5-v17\1619809322ppo",
     }
 
     performance_data = []
